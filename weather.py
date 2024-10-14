d0d5e0468960d4942bc85ed96c81b4e29fcbd081
Yiying <142329093+yiying909@users.noreply.github.com>
2024-10-14 16:24:15 -0400

    weather data extracted

---
+++ weather.py
@@ -0,0 +1,31 @@
+# Importing necessary libraries
+import requests
+from bs4 import BeautifulSoup
+
+# Enter city name
+city = input("Enter where you at? ")
+
+# Creating URL and making requests instance
+url = "https://www.google.com/search?q=" + "weather" + city
+html = requests.get(url).content
+
+# Getting raw data using BeautifulSoup
+soup = BeautifulSoup(html, 'html.parser')
+
+# Extracting the temperature
+temp = soup.find('div', attrs={'class': 'BNeawe iBp4i AP7Wnd'}).text
+
+# Extracting the time and sky description
+str_ = soup.find('div', attrs={'class': 'BNeawe tAd8D AP7Wnd'}).text
+data = str_.split('\n')
+time = data[0]
+sky = data[1]
+
+
+# Printing the extracted weather data
+print("Temperature is:", temp)
+print("Time:", time)
+print("Sky Description:", sky)
+
+
+
