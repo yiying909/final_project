b1b60473f3d1b724387d2188fb2c22ce639f7f18
Yiying <142329093+yiying909@users.noreply.github.com>
2024-10-16 13:53:05 -0400

    rewrite the code and add fahrenheit and celsius temp

--- weather.py
+++ weather.py
@@ -1,31 +1,24 @@
-# Importing necessary libraries
+# import libraries we need
 import requests
 from bs4 import BeautifulSoup
 
-# Enter city name
-city = input("Enter where you at? ")
+# ask for location and get search result; determine header to access actual attritube on webpage
+header = {"User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/128.0.0.0 Safari/537.36"}
+location = input("where are you at? ")
+url = "https://www.google.com/search?q=" + "weather" + location
+html_content = requests.get(url, headers = header).content
 
-# Creating URL and making requests instance
-url = "https://www.google.com/search?q=" + "weather" + city
-html = requests.get(url).content
+# parse html into readable content
+content = BeautifulSoup(html_content, 'html.parser')
 
-# Getting raw data using BeautifulSoup
-soup = BeautifulSoup(html, 'html.parser')
+# find actual info needed from content: temp in °F and °C
+temp_f = content.find('span', attrs={'class': 'wob_t q8U8x'}).text
+temp_c = content.find('span', id='wob_ttm').text
+real_time = content.find('div', attrs={'class': 'wob_dts'}).text
+description = content.find('div', attrs={'class': 'wob_dcp'}).text
 
-# Extracting the temperature
-temp = soup.find('div', attrs={'class': 'BNeawe iBp4i AP7Wnd'}).text
+print(f'''Right now is {real_time},
+with temperature is {temp_f}°F/{temp_c}°C, and {description}
+''')
 
-# Extracting the time and sky description
-str_ = soup.find('div', attrs={'class': 'BNeawe tAd8D AP7Wnd'}).text
-data = str_.split('\n')
-time = data[0]
-sky = data[1]
-
-
-# Printing the extracted weather data
-print("Temperature is:", temp)
-print("Time:", time)
-print("Sky Description:", sky)
-
-
-
+# <span class="wob_t" id="wob_ttm" style="">12</span>
